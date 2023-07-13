e0f33de131719a85a087c1621428531ddee983e2
revione <revione@outlook.com>
2023-07-13 16:26:56 +0200

    vamo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const Title = ({ children }: { children: string }) => (
 
 export default function App() {
   return (
-    <div className="w-full p-10 flex justify-center print:p-0">
+    <div className="w-full md:p-10 flex justify-center print:p-0">
       <div className="shadow-2xl shadow-cyan-500/50 print:shadow-none">
         {/* Header */}
         <div
